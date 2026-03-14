f348d274e0c6ff02f571122dba46302e640fdd3c
Clkido <128237039+Clkido@users.noreply.github.com>
2026-03-14 21:26:22 +0100

    s

---
+++ devix-project/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DevixApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DevixApp />
+  </React.StrictMode>
+);
