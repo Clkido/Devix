53fa0c84e0504048efc1e33b0fde86d65d349ca2
Clkido <128237039+Clkido@users.noreply.github.com>
2026-03-21 21:02:28 +0100

    Delete devix-v3 directory

--- devix-v3/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import DevixApp from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <DevixApp />
-  </React.StrictMode>
-);
